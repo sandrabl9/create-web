54dabad75113310306b7dab6a24567780da9d33f
Sandra <sandra.bretones@pccomponentes.com>
2022-06-15 17:43:38 +0200

    fetch colecciones

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export const Collections = () => {
-  return (
-    <div>Collections</div>
-  )
-}
+  
+  const [collections,setCollections] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      const response = await fetch(
+        "https://create-web-e8e99-default-rtdb.firebaseio.com/collections.json"
+      );
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const responseData = await response.json();
+      const loadedCollections = [];
+
+      for (const key in responseData) {
+        loadedCollections.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          
+        });
+      }
+      setCollections(loadedCollections);
+      setIsLoading(false);
+    };
+    ;
+      fetchCollections().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);});
+  }, []);
+
+  if (isLoading) {
+    return ( <section>
+      <p>Loading...</p>
+    </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section >
+      <p>{httpError}</p>
+    </section>
+    )
+
+  }
+
+  const collectionsList = collections.map(col =>({
+    id: col.id,
+    name: col.name,
+    description: col.description,
+    
+
+  }))
+
+
+  return <div>
+    Collections 
+
+    {
+      collectionsList.map( c => 
+        <ol key={c.id}>
+          {c.name}
+        </ol>
+      )
+    }
+
+  </div>;
+};
